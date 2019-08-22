807bc0fed27716de1733b0fa50fb1b0beb318e61
Heath Naylor <heath@bythepixel.com>
2019-08-21 20:04:58 -0600

    Fixes

--- index.js
+++ index.js
@@ -3,38 +3,32 @@ let randomizer = function(array) {
    let randNumber = Math.floor(Math.random() * Math.floor(total))
    return randNumber;
 }
+
 let returnAll = function(inputQuote, inputAuthor) {
    const totalQuotes = data.results.length;
    for (i = 0; i < totalQuotes; i++) {
        inputQuote.insertAfter(document.createElement('p'));
-       // inputQuote.innerHTML = data.results[rand].content;
-       // inputAuthor.innerHTML = datad = randomizer(data);
-           // quote.innerHTML = data.results[rand].content;
-           // author.innerHTML = data.results[rand].author;
    }
 }
+
 const loginForm = document.getElementById('login-form')
-loginForm.addEventListener('submit', evt => {
-   evt.preventDefault();
+loginForm.addEventListener('submit', e => {
+   e.preventDefault();
    const authorSearch = loginForm.querySelector('input[type="text"]').value;
    const quote = document.getElementById('random-quote');
    const author = document.getElementById('author');
    const checkedForAll = document.getElementById("allQuotes").checked;
-   console.log(checkedForAll);
    fetch(`https://api.quotable.io/quotes?author=${authorSearch}`)
        .then(response => response.json())
        .then(data => {
-           if (checkedForAll == true) {
-               // returnAll(quote, author);
-               console.log(data.results);
-               const newElem = document.createElement('div');
-               newElem.innerHTML = '<p>testo!</p>';
-               const ref = document.querySelector('p.before');
-               insertAfter(newElem, ref);
+           if (checkedForAll) {
+               data.results.forEach(result => {
+                   const newElem = document.createElement('div');
+                   newElem.innerHTML = result.content;
+                   const ref = document.querySelector('p');
+                   ref.parentNode.insertBefore(newElem, ref.nextSibling);
+               })
            }
-           // const rand = randomizer(data);
-           // quote.innerHTML = data.results[rand].content;
-           // author.innerHTML = data.results[rand].author;
        })
        .catch(error => { quote.innerHTML = "Please try a different search query."; } );
 });
